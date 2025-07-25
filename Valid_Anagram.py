7aa30c74719bac0f435e67b37d545553a6a444ee
Manas1605 <manasbhajgawre05@gmail.com>
2025-07-25 18:37:56 +0530

    updated

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -1,4 +1,6 @@
-
+# Counter gives ouput 
+# Get the most common element and its frequency as a list of one tuple
+        # Example: [(2, 5)] → number 2 appears 5 times
 from collections import Counter
 def anagram(s:str,t:str):
     Scount = Counter(s)
